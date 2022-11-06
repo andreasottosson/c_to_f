61c9a3a7aa38c3538a2eefbfbeb7ded6ec42865d
Andreas Ottosson <andreasottosson@me.com>
2022-11-06 17:25:48 +0100

    Initial commit..

---
+++ README.md
@@ -0,0 +1,3 @@
+# Convert Celsius to Fahrenheit
+
+Simple Rust code to convert Celsius to Fahrenhet.
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    println!("Enter the temp in celsius:");
+
+    let mut c = String::new();
+
+    io::stdin()
+        .read_line(&mut c)
+        .expect("Error reading input.");
+
+    let c: f64 = c.trim().parse().expect("Please type a number!");
+
+    let f = (c * 1.8) + 32.0;
+
+    println!("Fahrenheit: {:.1}", f);
+}
